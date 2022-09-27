2c73008cbdc1fc9388148af9ade3354fd96dad81
Bibliya <seongkyeong.seo@gmail.com>
2022-09-27 17:15:41 +0900

    Update: 타입 단언 & 타입 가드

--- about-typescript/example/9_type-inference.ts
+++ about-typescript/example/9_type-inference.ts
@@ -1,26 +1,31 @@
-var a = 'a';
+// 타입 추론 기본 1
+let a = "a";
 
-function logA(a = 'a') {
-  var b = 10;
-  return b;
-}
+function logA(a = "a") {
+  let b = 10;
+  return a + b;
+} // a10
 
+// 타입 추론 기본 2
 interface Dropdown<T> {
-  value: T
+  value: T;
   title: string;
 }
-var items: Dropdown<number> = {
-  value: 10,
-  title: 'a'
-}
+const items: Dropdown<string> = {
+  value: "value",
+  title: "a",
+};
 
 interface DetailedDropdown<T> extends Dropdown<T> {
   description: string;
   tag: T;
 }
-var detailItems: DetailedDropdown<number> = {
-  value: 'hi',
-  title: 'a',
-  description: 'b',
-  tag: 'c'
-}
+const detailItems: DetailedDropdown<number> = {
+  value: 200,
+  title: "a",
+  description: "b",
+  tag: 10,
+};
+
+// Best Common Type
+const arr = [1, 2, true, true, "a"];

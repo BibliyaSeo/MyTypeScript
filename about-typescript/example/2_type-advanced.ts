7970e6086aa13ade079f289abea0e2bcd2a5a715
Bibliya <seongkyeong.seo@gmail.com>
2022-09-27 14:38:16 +0900

    Update: type & enum & interface & todo

--- about-typescript/example/2_type-advanced.ts
+++ about-typescript/example/2_type-advanced.ts
@@ -1,3 +1,20 @@
+// 숫자형 이넘(Enum)
+enum Shoes {
+  Nike,
+  Adidas,
+}
+
+let myShoes = Shoes.Nike;
+console.log(myShoes); // 0
+
+// 문자형 이넘(Enum)
+enum ShoesBrand {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+
+console.log(ShoesBrand.Nike); // '나이키'
+
 // 이넘(Enum)
 enum Avengers {
   Capt,
@@ -7,7 +24,7 @@ enum Avengers {
 const myHero = Avengers.Capt;
 
 // any
-let a: any = 'hi';
+let a: any = "hi";
 a = 20;
 a = false;
 
@@ -17,12 +34,27 @@ function sayHi(): void {
 }
 
 // 타입 추정 (type assertion)
-let str = '이건 문자열 타입이 됩니다.';
+let str = "이건 문자열 타입이 됩니다.";
 
 let num: number;
-num = 'str' as any;
+num = "str" as any;
 
-const divElement = document.querySelector('#app');
+const divElement = document.querySelector("#app");
 divElement.innerHTML;
 // const empty = document.querySelector('#app') as null;
 // empty.innerHTML;
+
+// 예제
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다!");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다");
+  }
+}
